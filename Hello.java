2b48fd55e1b449e31c74350e15f06d884e45bebd
NiharikaBariar <niharikaa2050@gmail.com>
2021-02-11 16:10:27 +0530

    added a new print line with java version

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@
 public class Hello {
  public static void main(String args[]){
      System.out.println("Hello World");
+//System.out.println("Welcome to Java 1.8");
+System.out.println("Welcome to Java 1.11");
  }
 }
