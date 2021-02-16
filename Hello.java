93b794d91cd7c2474815d42718e564b06d35842f
NiharikaBariar <niharikaa2050@gmail.com>
2021-02-16 14:45:32 +0530

    modified hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,5 @@
 public class Hello {
  public static void main(String args[]){
      System.out.println("Hello World");
-//System.out.println("Welcome to Java 1.8");
-System.out.println("Welcome to Java 1.11");
  }
 }
